95045fbe7f3439b8aa3894e8ca3bff190a5e4e9d
Victor R. <joaorivaroli.m@gmail.com>
2026-09-01 20:37:37 -0300

    Refactor dictionary management functions
    Updated functions for managing a dictionary, including adding, editing, and removing keys. Improved user feedback and handling of existing keys.

--- teste.py
+++ teste.py
@@ -6,38 +6,50 @@ def zerar_dicionario(dicio: dict) -> None:
     if len(dicio) == 0:
         print("--------Conteúdo do dicionario \n \tVAZIO!")
     else:
+        print(">>>>>> Dicionario zerado!")
         dicio = {}
         
 def exibir_dicionario(dicio: dict) -> None:
-    print("\n------- Conteudo do dicionario")
+    print("\n-------Conteudo do dicionario")
     for k, v in dicio.items():
         print(f"{k}.........: {v}")
     print("------------------------------")
+    input("Precione algo para continuar...")
 
 def adicionar_key(dicio: dict) -> None:
         nome_key = input("Nome da key: ")
-        print("""
-        1 - int
-        2 - float
-        3 - str
-        4 - bool
-        """)
-        tipo_dado= int(input("\tEscolha: "))
-        match (tipo_dado):
-                case 1:
-                    conteudo_key = int(input("Digite o conteudo: "))
-                    dicio[nome_key] = conteudo_key
-                case 2:
-                    conteudo_key = float(input("Digite o conteudo: "))
-                    dicio[nome_key] = conteudo_key
-                case 3 :
-                    conteudo_key = input("Digite o conteudo: ")
-                    dicio[nome_key] = conteudo_key
-                case 4:
-                    conteudo_key = bool(input("Digite o conteudo: "))
-                    dicio[nome_key] = conteudo_key
-                case _ :
-                    print("Opçao invalida")
+        if nome_key in dicio:
+            print(f">>>>> A key '{nome_key}' já existe!")
+        else:    
+            print("""
+            1 - int
+            2 - float
+            3 - str
+            4 - bool
+            """)
+            tipo_dado= int(input("\tEscolha: "))
+            conteudo_key = input("Digite o conteudo: ")
+            
+            match (tipo_dado):
+                    case 1:
+                        if conteudo_key == "":
+                            dicio[nome_key] = 0
+                        else:
+                            dicio[nome_key] = int(conteudo_key)
+                    case 2:
+                        if conteudo_key == "":
+                            dicio[nome_key] = 0.0
+                        else:
+                            dicio[nome_key] = float(conteudo_key)
+                    case 3 :
+                        dicio[nome_key] = conteudo_key
+                    case 4:
+                        if conteudo_key == "":
+                            dicio[nome_key] = False
+                        else:
+                            dicio[nome_key] = bool(conteudo_key)
+                    case _ :
+                        print("Opçao invalida")
 
 def editar_value(dicio: dict) -> None:
     if len(dicio) == 0:
@@ -87,30 +99,36 @@ def remover_key(dicio: dict) -> None:
         if chave_selecionada != 0:
             del dicio[chave_selecionada]
             exibir_dicionario(dicio)
+            input("Precione algo para continuar...")
         else:
             print(f">>>>> '{nun_chave}' é um número de chave inválido!")
 
-while True:
-    print("""
-        0 - Sair
-        1 - Zerar dicionario
-        2 - Adicionar uma key
-        3 - Editar value
-        4 - Remover uma key
-        5 - Exibir dicionario
-    """)
-    escolha = int(input("\tEscolha: "))
+def menu_principal(dicionario: dict) -> None:
+    while True:
+        print("""
+            M E N U
+            -------
+            0 - Sair
+            1 - Zerar dicionario
+            2 - Adicionar uma key
+            3 - Editar value
+            4 - Remover uma key
+            5 - Exibir dicionario
+        """)
+        escolha = int(input("\tEscolha: "))
 
-    match (escolha):
-        case (0):
-            break
-        case 1:
-            zerar_dicionario(dicionario)
-        case 2:
-            adicionar_key(dicionario)
-        case 3:
-            editar_value(dicionario)
-        case 4:
-            remover_key(dicionario)
-        case 5:
-            exibir_dicionario(dicionario)
+        match (escolha):
+            case (0):
+                break
+            case 1:
+                zerar_dicionario(dicionario)
+            case 2:
+                adicionar_key(dicionario)
+            case 3:
+                editar_value(dicionario)
+            case 4:
+                remover_key(dicionario)
+            case 5:
+                exibir_dicionario(dicionario)
+
+menu_principal(dicionario)
